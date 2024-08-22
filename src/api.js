c82e482e443b8511dcbdc83e47a4e5846ff43cec
AniketR9 <rajeshaniket13@gmail.com>
2024-08-22 05:47:00 +0530

    Completed

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "3d66169605msh9da96aa5a30c9bfp1aa74ajsnc92cafad8635",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "10b6e64ec079be0471237b3df12cc11a"; 
